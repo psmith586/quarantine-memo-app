20b585723b5bed5acd722800818ec10b32ea1d0b
psmith586 <psmith586@yahoo.com>
2020-03-22 15:43:28 -0700

    first commit (cra/boiler plate/pages adding)

---
+++ src/pages/user.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class user extends Component{
+  render () {
+    return (
+      <div>
+        <h1>user page</h1>
+      </div>
+    )
+  }
+}
+
+export default user
